3d0df13723c494907f569306d71e3e38279f22bf
Lucas Oliveira <lucas.oliveirasbs@gmail.com>
2022-02-23 14:43:48 -0300

    Create sumIf function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-export default function sumIf() {
-  return []
+export default function sumIf(values, fn, initial = 0) {
+ return values.reduce((acc, value) => {
+  if (fn(value)) {
+    return acc + value
+  }
+
+  return acc
+ }, initial)
 }
--- src/index.test.js
+++ src/index.test.js
@@ -3,9 +3,34 @@ import sumIf from '.'
 describe('sumIf()', () => {
   test('with empty array', () => {
     const actual = []
-    const expected = []
+    const expected = 0
     const fn = () => true
 
     expect(sumIf(actual, fn)).toEqual(expected)
+  })
+
+  test('sum values greather than or equal 300', () => {
+    const actual = [100, 200, 300, 400, 500]
+    const expected = 1200
+    const fn = value => value >= 300
+
+    expect(sumIf(actual, fn)).toEqual(expected)
+  })
+
+  test('sum odd values', () => {
+    const actual = [1, 2, 3, 4, 5, 6]
+    const expected = 12
+    const fn = value => value % 2 === 0
+
+    expect(sumIf(actual, fn)).toEqual(expected)
+  })
+
+  test('Concact string wth length igual 2', () => {
+    const actual = ['aa', 'bbb', 'cc', 'dd', 'e']
+    const expected ='aaccdd'
+    const initial = ''
+    const fn = value => value.length === 2
+
+    expect(sumIf(actual, fn, initial)).toEqual(expected)
   })
 })
